40363a6684289ccd0e87cf90b8c73ed85afc86c3
Raúl González Vicente <rgonzale@viewnext.com>
2017-12-25 22:50:21 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    // node >= v7, sino ponemos este tercer argumento obtenemos
+    // un warning de deprecation
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenence.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+  // return 'test';
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+// register handler for http request, a route
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    welcomeMessage: 'Welcome to my website!',
+    pageTitle: 'Home page',
+    // currentYear: new Date().getFullYear()
+  });
+})
+
+
+app.get('/about', (req, res) => {
+  // res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    // currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+//bind the app to a port in our machine
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
